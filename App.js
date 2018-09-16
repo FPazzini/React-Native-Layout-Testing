abb5cb41658e50ec7dd550c37bac0539ddfc4650
Fabio Pazzini <fpazzini94@gmail.com>
2018-09-16 20:25:59 +0200

    First grid layout, insipired from Aitbnb

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View, FlatList } from 'react-native';
+import SquaresInterface from './components/SquaresInterface'
+import { Header } from './components/common'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <Header headerText="GridList" />
+        <SquaresInterface />
+      </View>
+    );
+  }
+}
---
+++ components/SquaresInterface.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+import { View, Text, FlatList, Image, ScrollView, TouchableHighlight } from 'react-native'
+import { Card, CardSection, Button } from './common'
+
+const numCols = 2
+
+class SquaresInterface extends Component {
+
+    renderList = ({ item }) => {
+        return (
+            <View>
+                <Card>
+                    <TouchableHighlight onPress={() => alert("Cliccato: "+item.id)}>
+                        <View>
+                            <View style={styles.innerViewStyle}>
+                                <Image style={styles.image} source={item.avatar} />
+                            </View>
+                            <View style={styles.textViewStyle}>
+                                <Text style={styles.descriptionStyle}>
+                                    {item.description}
+                                </Text>
+                            </View>
+                        </View>
+                    </TouchableHighlight>
+                </Card>
+            </View>
+
+        );
+    
+    }
+    render() {
+        return (
+            <View style={styles.viewStyle}>
+                <FlatList
+                data={
+                    [
+                        {
+                            id: '1',
+                            avatar: require('../assets/example.jpg'), 
+                            description: 'primo'
+                        }, 
+                        {
+                            id: '2',
+                            avatar: require('../assets/icebridge.jpg'),
+                            description: 'secondo'
+                        }, 
+                        {
+                            id: '3',
+                            avatar: require('../assets/squalo-bianco.jpg'),
+                            description: 'terzo'
+                        }, 
+                        {
+                            id: '4',
+                            avatar: require('../assets/squalo-toro.jpg'),
+                            description: 'quarto'
+                        }, 
+                        {
+                            id: '5',
+                            avatar: require('../assets/paris.jpg'),
+                            description: 'quinto'
+                        }, 
+                        {
+                            id: '6',
+                            avatar: require('../assets/paris.jpg'),
+                            description: 'sesto'
+                        }, 
+                        {
+                            id: '7',
+                            avatar: require('../assets/paris.jpg'),
+                            description: 'settimo'
+                        }, 
+                        {
+                            id: '8',
+                            avatar: require('../assets/paris.jpg'),
+                            description: 'ottavo'
+                        }, 
+                        {
+                            id: '9',
+                            avatar: require('../assets/paris.jpg'),
+                            description: 'nono'
+                        }, 
+                        {
+                            id: '10',
+                            avatar: require('../assets/paris.jpg'),
+                            description: 'decimo'
+                        }
+                    ]
+                }
+                renderItem={this.renderList}
+                numColumns={numCols}
+                keyExtractor={item => item.id}
+            />
+
+            </View>
+        )
+    }
+}
+
+const styles = {
+    viewStyle: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'stretch',
+    },
+    innerViewStyle: {
+        width: '100%',
+        height: '80%'
+    },
+    descriptionStyle: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    textViewStyle: {
+        alignItems: 'flex-start'
+    }
+}
+
+export default SquaresInterface
